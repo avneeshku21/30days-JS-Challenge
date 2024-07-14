42ad024855d96fa1712b9263261864ad281480e8
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-14 13:19:16 +0530

    update

---
+++ Day1/Operator/operator.js
@@ -0,0 +1,34 @@
+// Arithmetic Operation
+let num=10
+let num2=20
+result=num+num2
+console.log('Result ' ,result)
+
+let sub1=10
+let sub2=20
+sub=sub1-sub2
+console.log('Result ' ,sub)
+
+// let x = 5;
+// let y = 3;
+// let z = x * y;
+
+// let x = 8;
+// let y = 2;
+// let z = x / y;
+
+// let x = 9;
+// let y = 2;
+// let z = x % y;
+
+//Assignment Operator
+let as = 10;
+as += 5;
+console.log(as)
+
+let ab = 10;
+ab &= 5;
+
+
+let z = 10;
+z |= 5;
