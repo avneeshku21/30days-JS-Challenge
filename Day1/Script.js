61fc78e68d8147fdfaaecc2e395d383442a041da
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-14 13:03:40 +0530

    first commit

---
+++ Day1/Script.js
@@ -0,0 +1,33 @@
+// Variable Declartion
+//Task 1 Declare A varibale using var assign it a number and log the value to the console
+
+//Task 2 Declare A varibale using let assign it a string and log the value to the console
+
+var a =10
+console.log(a)
+
+let str= "Avneesh"
+console.log(str)
+
+//*******Constant Declartion */
+
+//Task 3 Declare A varibale using const assign it a boolean  value and log the value to the console
+const isTrue = true;
+console.log(isTrue);
+
+
+//Data types
+let num=10
+console.log("Number ka type : ",typeof(num))
+let b="hello"
+console.log("string ka type : ",typeof(b))
+let isfalse=false
+console.log("boolen ka type : ",typeof(isfalse))
+const user={
+    name: "avneesh",
+    age:"19"
+}
+
+console.log("Object ka type : ",typeof(user))
+let arr=[1,2,3,4,5,5]
+console.log("Array ka type : ",typeof(arr))
