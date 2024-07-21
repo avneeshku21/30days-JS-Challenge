061a37e2012990412b5ea1c997d6ebd9e3a89a33
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-21 22:30:39 +0530

    update

---
+++ Day4/question2.js
@@ -0,0 +1,12 @@
+// program to generate a multiplication table
+
+const number = parseInt(prompt('Enter an integer: '));
+
+//creating a multiplication table
+for(let i = 1; i <= 10; i++) {
+
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
+}
